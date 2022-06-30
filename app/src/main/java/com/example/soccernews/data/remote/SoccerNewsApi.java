4006e7a793b9acfc6bd6111528cc7f5849a28d4d
João Gabriel Correa <joaogabrielcorrea1@hotmail.com>
2022-06-30 14:47:29 -0300

    Inclusão Retrofit e Picasso

---
+++ app/src/main/java/com/example/soccernews/data/remote/SoccerNewsApi.java
@@ -0,0 +1,16 @@
+package com.example.soccernews.data.remote;
+
+
+import java.util.List;
+
+import com.example.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+public interface SoccerNewsApi {
+
+    @GET("news.json")
+    Call<List<News>> getNews();
+
+}
--- app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
@@ -4,28 +4,53 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.soccernews.data.remote.SoccerNewsApi;
 import com.example.soccernews.domain.News;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
 
-        //TODO Remover Mock de Noticias
-        List<News> news = new ArrayList<>();
-        news.add(new News( "Desfalque Importante" ,  "Simply dummy text of the printing and typesetting industry. " ));
-        news.add(new News( "Mulher no Futebol" ,  "Simply dummy text of the printing and typesetting industry. " ));
-        news.add(new News( "O futebol e a Superação Feminina" ,  "Simply dummy text of the printing and typesetting industry. " ));
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://joaogabrielcorrea.github.io/soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        this.news.setValue(news);
+        api = retrofit.create(SoccerNewsApi.class);
+        this.findNews();
     }
 
-    public LiveData<List<News>> getNews() {
-        return this.news;
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if (response.isSuccessful()) {
+                    news.setValue(response.body());
+
+                } else {
+                    //TODO Pensar em estrategia de tratamento de erros
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+
+            }
+        });
     }
+
+    public LiveData<List<News>> getNews() {return this.news;}
 }
